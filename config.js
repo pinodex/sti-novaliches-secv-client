f6c8009e97add6ea6aabc855363d49c9deac0e73
Raphael Marco <raphaelmarco@outlook.com>
2017-01-29 23:22:07 +0800

    Initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  socket: 'http://localhost:3333'
+}
---
+++ main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+/*!
+ * STI College Novaliches
+ * SEC Voting System Client
+ *
+ * Copyright 2017, Raphael Marco <raphaelmarco@outlook.com>
+ */
+
+const {app, BrowserWindow} = require('electron'),
+      path = require('path')
+
+const iconPath = 'file://' + path.join(__dirname, 'assets', 'icon.ico')
+const entryPage = 'file://' + path.join(__dirname, 'views', 'index.html')
+
+let _window
+
+app.on('ready', () => {
+  _window = new BrowserWindow({
+    title: 'SECV Client',
+    icon: iconPath,
+    kiosk: process.env.NODE_ENV != 'dev'
+  })
+
+  _window.on('closed', () => {
+    _window = null
+  })
+
+  _window.loadURL(entryPage)
+});
+
+app.on('browser-window-created', (e, window) => {
+  if (process.env.NODE_ENV != 'dev') {
+    window.setMenu(null)
+  }
+});
